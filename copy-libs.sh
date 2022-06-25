7127ab668ec7b324ffb45dfa2aea515aaf900bd5
wiredquill <erin@suse.com>
2022-06-25 05:53:27 -0700

    Create copy-libs.sh

---
+++ copy-libs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+DEV_DIR="/Users/erin/SynologyDrive/Personal/CurciutPython/adafruit-circuitpython-bundle-7.x-mpy-20220623/lib"
+MP_DIR="/Volumes/CIRCUITPY/lib"
+
+LIBS=("adafruit_matrixportal" \
+"adafruit_portalbase" \
+"adafruit_esp32spi" \
+"adafruit_bus_device" \
+"adafruit_requests.mpy" \
+"adafruit_fakerequests.mpy" \
+"adafruit_io" \
+"adafruit_bitmap_font" \
+"adafruit_display_text" \
+#"adafruit_wsgi" \
+#"adafruit_minimqtt" \
+#"adafruit_lis3dh.mpy" \
+"neopixel.mpy" )
+
+for L in ${LIBS[@]}; do
+  echo $L " to " $MP_DIR
+  cp -R $DEV_DIR/$L $MP_DIR/.
+done
